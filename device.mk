c118e1f7a071f58661ba35fb02d65f1b9846f9f6
Michael Bestas <mkbestas@lineageos.org>
2018-05-14 22:15:24 +0300

    op3: Don't build tinymix
    * This tool can be built manually for debugging cases,
      there is no need to ship it in production
    
    Change-Id: I89630dea56b8d82ebef964a2afd5465e87942d45
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- device.mk
+++ device.mk
@@ -121,7 +121,7 @@ PRODUCT_PACKAGES += \
     libqcomvisualizer \
     libqcomvoiceprocessing \
     libvolumelistener \
-    tinymix
+    libtinycompress
 
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
