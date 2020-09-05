PixelExperience-Devices/device_asus_X00TD

X00TD: add prebuilt Gcam Go
Signed-off-by: alexmercer09 <supremegod.of.war@gmail.com>

prebuilts/Android.mk
@@ -1,0 +1,12 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := GoogleCameraGo
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := system/priv-app/GoogleCameraGo/GoogleCameraGo.apk
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_PATH := $(TARGET_OUT)/priv-app
+LOCAL_OVERRIDES_PACKAGES := SnapdragonCamera Camera2 Snap Snap2
+include $(BUILD_PREBUILT)

aquariusjr10/android_device_oneplus_oneplus3_10

prebuilts/Android.mk
@@ -1,0 +1,12 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := GoogleCamera
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := system/priv-app/GoogleCamera/GoogleCamera.apk
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_PATH := $(TARGET_OUT)/priv-app
+LOCAL_OVERRIDES_PACKAGES := SnapdragonCamera Camera2 Snap Snap2
+include $(BUILD_PREBUILT)