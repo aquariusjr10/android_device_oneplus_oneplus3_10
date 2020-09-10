f62cc8d2e87e3ca1c4eeab277746d20c2f92d4c3
Deepak jr <deepakjeganathan@gmail.com>
2020-09-10 13:10:49 +0100

    Remove duplicate overlays

--- bliss_oneplus3.mk
+++ bliss_oneplus3.mk
@@ -36,3 +36,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 BUILD_FINGERPRINT := "OnePlus/OnePlus3/OnePlus3:9/PKQ1.181203.001/1907311932:user/release-keys"
 
 TARGET_VENDOR := oneplus
+
+#Bliss build details
+BLISS_BUILDTYPE=OFFICIAL
